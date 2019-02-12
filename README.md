85d7493984dc16d1a2791279a8dbbf669b225982
vissejul <vissejul@students.zhaw.ch>
2019-02-12 16:42:42 +0100

    made csv output and added a plot

--- README.md
+++ README.md
@@ -1,17 +1,7 @@
 # cloc-uni
 
-python script that Counts Lines of Code written during uni
+python script that Counts Lines of Code written during my 3 years at uni
 
 Output: 07.02.2019
 
-| Language:  | Linesof Code: |
-| ---------- | ------------- |
-| C          | 12172         |
-| Java       | 24701         |
-| Python     | 3365          |
-| Assembly   | 2777          |
-| SQL        | 878           |
-| JavaScript | 21048         |
-| HTML       | 20234         |
-| CSS        | 2227          |
-| Sass       | 1775          |
+![Plot](plot.png)
--- readfile.py
+++ readfile.py
@@ -1,11 +1,15 @@
 from os import rename, listdir
 import subprocess
 
+import csv
+
+import plotly
+
 foutputname = 'count.txt'
 outputfolder = "counts"
 
 dict = {}
-
+list_wanted_language= ["C", "Java","Python" ,"Assembly","SQL","JavaScript","HTML","CSS","Sass"]
 fnames = listdir(outputfolder)
 
 for fname in fnames:
@@ -24,9 +28,9 @@ for fname in fnames:
             if(words[0] in dict):
                 list = dict[words[0]]["subjects"]
                 list.append(name)
-                dict[words[0]]= {"subjects": list,"fileCount": dict[words[0]]["fileCount"]+int(words[1]),"code":dict[words[0]]["code"]+int(words[4])}
+                dict[words[0]]= {"language": words[0],"subjects": list,"fileCount": dict[words[0]]["fileCount"]+int(words[1]),"code":dict[words[0]]["code"]+int(words[4])}
             else:
-                dict[words[0]]= {"subjects": [name],"fileCount":int(words[1]),"code": int(words[4])}
+                dict[words[0]]= {"language": words[0],"subjects": [name],"fileCount":int(words[1]),"code": int(words[4])}
 
 
 print("Language:" +'\t\t' + "Lines of Code:")
@@ -44,3 +48,15 @@ print("JavaScript"+"\t\t",dict["JavaScript"]["code"])
 print("HTML"+"\t\t\t",dict["HTML"]["code"])
 print("CSS"+"\t\t\t",dict["CSS"]["code"])
 print("Sass"+"\t\t\t",dict["Sass"]["code"])
+
+
+# Write to csv
+with open('count.csv', mode='w') as csv_file:
+    fieldnames = ['language', 'subjects', 'fileCount', 'code']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    writer.writeheader()
+
+    for x in dict:
+        if x in list_wanted_language:
+            writer.writerow(dict[x])
